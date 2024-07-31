4f9b10a56f21c4f17593052cee20b3447cbacebb
ChinmaynBarhate <chinmaybarhate83@gmail.com>
2024-07-31 23:36:52 +0530

    Update Kalka.java

--- first-app/src/main/java/bca/Kalka.java
+++ first-app/src/main/java/bca/Kalka.java
@@ -5,8 +5,9 @@ public class Kalka {
 public static void main(String[] args) {
     int a = 10;
     int b = 20;
+    int c = 30;
     int sum;
-    sum = a-b;
+    sum = a+b+c;
     System.out.println(sum); 
 }
 }
