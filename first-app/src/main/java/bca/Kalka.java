f1dc359ed397f28441b5ec97a2a90f4631db5933
ChinmaynBarhate <chinmaybarhate83@gmail.com>
2024-07-31 23:39:57 +0530

    SecCommit

--- first-app/src/main/java/bca/Kalka.java
+++ first-app/src/main/java/bca/Kalka.java
@@ -6,8 +6,9 @@ public static void main(String[] args) {
     int a = 10;
     int b = 20;
     int c = 30;
+    int d = 40;
     int sum;
-    sum = a+b+c;
+    sum = a+b+c+d;
     System.out.println(sum); 
 }
 }
