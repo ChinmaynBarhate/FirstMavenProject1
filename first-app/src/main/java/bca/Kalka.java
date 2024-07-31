07dedcdc319dd0811b024c3e9933c8bdf62e639a
ChinmaynBarhate <chinmaybarhate83@gmail.com>
2024-07-31 23:11:57 +0530

    first commit

---
+++ first-app/src/main/java/bca/Kalka.java
@@ -0,0 +1,12 @@
+package bca;
+
+public class Kalka {
+
+public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int sum;
+    sum = a+b;
+    System.out.println(sum); 
+}
+}
